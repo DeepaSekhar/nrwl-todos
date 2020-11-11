a2a9fe60706f1d892c81666a44cc6dddd0be7a4b
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-11-11 21:42:45 +0000

    todos apps in progress

--- apps/todos-app/src/app/app.component.ts
+++ apps/todos-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from "./todo"
 
 @Component({
   selector: 'nrwl-todos-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos-app';
+  todoValue: string;
+  list: Todo[];
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+  addItem() {
+    console.log("add item")
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem)
+    }
+    this.todoValue = ""
+  }
+  deleteItem(id: number) {
+    console.log("delete is working");
+    this.list = this.list.filter(item => item.id !== id);
+  }
+
 }
